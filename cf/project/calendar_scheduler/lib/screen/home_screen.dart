b742648baeb49429a1c1642dc1f8cbb033d85b5b
Hoseong Kim <8536048@gmail.com>
2023-02-07 01:19:54 +0900

    캘린더 스케쥴러 앱

---
+++ cf/project/calendar_scheduler/lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:calendar_schedule/component/calendar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Calendar(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
