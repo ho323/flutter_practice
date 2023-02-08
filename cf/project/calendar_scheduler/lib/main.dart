a9fe4e04f55b82d33dca5193249bca34b76b0ed8
Hoseong Kim <8536048@gmail.com>
2023-02-08 12:00:00 +0900

    캘린더 스케쥴러 앱

--- cf/project/calendar_scheduler/lib/main.dart
+++ cf/project/calendar_scheduler/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:calendar_schedule/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
+  // 플러터 프레임워크가 실행 될때까지 기다리라는 함수
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(
     MaterialApp(
       theme: ThemeData(
--- cf/project/calendar_scheduler/lib/screen/home_screen.dart
+++ cf/project/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,8 +1,21 @@
 import 'package:calendar_schedule/component/calendar.dart';
+import 'package:calendar_schedule/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +24,27 @@ class HomeScreen extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              Calendar(),
+              Calendar(
+                selectedDay: selectedDay,
+                focusedDay: focusedDay,
+                onDaySelected: onDaySelected,
+              ),
+              SizedBox(height: 8.0),
+              TodayBanner(
+                selectedDay: selectedDay,
+                scheduleCount: 3,
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+      this.focusedDay = selectedDay; // 다른 달 날짜 클릭하면 달 focus 변경
+    });
   }
 }
