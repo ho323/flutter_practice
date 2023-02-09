b9a87bf1dce7fba8518cd75d3f04331b0b727ced
Hoseong Kim <8536048@gmail.com>
2023-02-09 12:00:00 +0900

    캘린더 스케쥴러 앱

--- cf/project/calendar_scheduler/lib/screen/home_screen.dart
+++ cf/project/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_schedule/component/calendar.dart';
+import 'package:calendar_schedule/component/schedule_card.dart';
 import 'package:calendar_schedule/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -33,6 +34,16 @@ class _HomeScreenState extends State<HomeScreen> {
               TodayBanner(
                 selectedDay: selectedDay,
                 scheduleCount: 3,
+              ),
+              SizedBox(height: 8.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ScheduleCard(
+                  startTime: 8,
+                  endTime: 14,
+                  content: '프로그래밍 공부하기',
+                  color: Colors.red,
+                ),
               ),
             ],
           ),
