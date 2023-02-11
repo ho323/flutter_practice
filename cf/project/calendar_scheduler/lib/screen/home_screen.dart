93078eb305e608353856c62dea2436c9984d2b51
Hoseong Kim <8536048@gmail.com>
2023-02-11 12:00:00 +0900

    캘린더 스케쥴러 앱

--- cf/project/calendar_scheduler/lib/screen/home_screen.dart
+++ cf/project/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:calendar_schedule/component/calendar.dart';
+import 'package:calendar_schedule/component/schedule_bottom_sheet.dart';
 import 'package:calendar_schedule/component/schedule_card.dart';
 import 'package:calendar_schedule/component/today_banner.dart';
+import 'package:calendar_schedule/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
       body: Center(
         child: SafeArea(
           child: Column(
@@ -36,28 +39,28 @@ class _HomeScreenState extends State<HomeScreen> {
                 scheduleCount: 3,
               ),
               SizedBox(height: 8.0),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ListView.separated(
-                    itemCount: 100,
-                    separatorBuilder: (context, index) {
-                      return SizedBox(height: 8.0);
-                    },
-                    itemBuilder: (context, index) {
-                      return ScheduleCard(
-                        startTime: 8,
-                        endTime: 14,
-                        content: '프로그래밍 공부하기 $index',
-                        color: Colors.red,
-                      );
-                    },
-                  ),
-                ),
-              ),
+              _ScheduleList(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (_) {
+            return ScheduleBottomSheet();
+          },
+        );
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(
+        Icons.add,
       ),
     );
   }
@@ -67,5 +70,32 @@ class _HomeScreenState extends State<HomeScreen> {
       this.selectedDay = selectedDay;
       this.focusedDay = selectedDay; // 다른 달 날짜 클릭하면 달 focus 변경
     });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+          itemCount: 100,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8.0);
+          },
+          itemBuilder: (context, index) {
+            return ScheduleCard(
+              startTime: 8,
+              endTime: 14,
+              content: '프로그래밍 공부하기 $index',
+              color: Colors.red,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
