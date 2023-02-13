cc350b0cf6d850f4667ee21039be37a050d1d54f
Hoseong Kim <8536048@gmail.com>
2023-02-13 12:00:00 +0900

    캘린더 스케쥴러 앱

--- cf/project/calendar_scheduler/lib/component/custom_text_field.dart
+++ cf/project/calendar_scheduler/lib/component/custom_text_field.dart
@@ -1,10 +1,15 @@
 import 'package:calendar_schedule/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
+  // true- 시간 / false- 내용
+  final bool isTime;
+
   const CustomTextField({
     required this.label,
+    required this.isTime,
     Key? key,
   }) : super(key: key);
 
@@ -20,15 +25,31 @@ class CustomTextField extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        TextField(
-          cursorColor: Colors.grey,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            filled: true,
-            fillColor: Colors.grey[300],
+        if (isTime) renderTextField(),
+        if (!isTime)
+          Expanded(
+            child: renderTextField(),
           ),
-        ),
       ],
+    );
+  }
+
+  Widget renderTextField() {
+    return TextField(
+      cursorColor: Colors.grey,
+      maxLines: isTime ? 1 : null,
+      expands: !isTime,
+      keyboardType: isTime
+          ? TextInputType.number // 키보드 타입 숫자로
+          : TextInputType.multiline,
+      inputFormatters: isTime
+          ? [FilteringTextInputFormatter.digitsOnly] // 숫자만 입력 가능
+          : [],
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.grey[300],
+      ),
     );
   }
 }
