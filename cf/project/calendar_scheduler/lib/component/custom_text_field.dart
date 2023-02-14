bc40f78c59acbbb07832bf4d0fb62e7fa7efdd2a
Hoseong Kim <8536048@gmail.com>
2023-02-14 23:28:47 +0900

    캘린더 스케쥴러 앱

--- cf/project/calendar_scheduler/lib/component/custom_text_field.dart
+++ cf/project/calendar_scheduler/lib/component/custom_text_field.dart
@@ -35,7 +35,12 @@ class CustomTextField extends StatelessWidget {
   }
 
   Widget renderTextField() {
-    return TextField(
+    return TextFormField(
+      // null이 return 되도 error가 없다.
+      // error가 있으면 error를 string 값으로 return.
+      validator: (String? val) {
+        return 'test';
+      },
       cursorColor: Colors.grey,
       maxLines: isTime ? 1 : null,
       expands: !isTime,
--- cf/project/calendar_scheduler/lib/main.dart
+++ cf/project/calendar_scheduler/lib/main.dart
@@ -1,18 +1,42 @@
+import 'package:calendar_schedule/database/drift_database.dart';
 import 'package:calendar_schedule/screen/home_screen.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() async{
+const DEFAULT_COLORS = [
+  'F44336', // 빨
+  'FF9800', // 주
+  'FFEB3B', // 노
+  'FCAF50', // 초
+  '2196F3', // 파
+  '3F51B5', // 남
+  '9C27B0', // 보
+];
+
+void main() async {
   // 플러터 프레임워크가 실행 될때까지 기다리라는 함수
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
 
+  final database = LocalDatabase();
+
+  final colors = await database.getCategoryColors();
+
+  if (colors.isEmpty) {
+    for (String hexCode in DEFAULT_COLORS) {
+      await database.createCategoryColor(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode),
+        ),
+      );
+    }
+  }
+
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'NotoSans'
-      ),
+      theme: ThemeData(fontFamily: 'NotoSans'),
       home: HomeScreen(),
     ),
   );
