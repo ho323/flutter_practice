8e38736756ba3b842495e0aa5207bcff75deaa37
Hoseong Kim <8536048@gmail.com>
2023-02-21 12:00:00 +0900

    캘린더 스케줄러 앱

--- cf/project/calendar_scheduler/lib/database/drift_database.dart
+++ cf/project/calendar_scheduler/lib/database/drift_database.dart
@@ -37,14 +37,21 @@ class LocalDatabase extends _$LocalDatabase {
     ]);
 
     query.where(schedules.date.equals(date));
+    query.orderBy([
+      // asc -> ascending 오름차순
+      // desc -> descending 내림차
+      OrderingTerm.asc(schedules.startTime),
+    ]);
 
     return query.watch().map(
-          (rows) => rows.map(
-            (row) => ScheduleWithColor(
-              schedule: row.readTable(schedules),
-              categoryColor: row.readTable(categoryColors),
-            ),
-          ).toList(),
+          (rows) => rows
+              .map(
+                (row) => ScheduleWithColor(
+                  schedule: row.readTable(schedules),
+                  categoryColor: row.readTable(categoryColors),
+                ),
+              )
+              .toList(),
         );
   }
 
