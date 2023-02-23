0cbccfe23191450d7f5b95cc5560f213dbea8db7
Hoseong Kim <8536048@gmail.com>
2023-02-23 20:41:48 +0900

    이론 Scrollable Widget

---
+++ cf/theory/scrollable_widget/lib/screen/grid_view_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widget/const/colors.dart';
+import 'package:scrollable_widget/layout/main_layout.dart';
+
+class GridViewScreen extends StatelessWidget {
+  List<int> numbers = List.generate(100, (index) => index);
+
+  GridViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'GirdViewScreen',
+      body: renderMaxExtent(),
+    );
+  }
+
+  // 1. 한 번에 다 그림
+  Widget renderCount() {
+    return GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 12.0,
+      mainAxisSpacing: 12.0,
+      children: numbers
+          .map(
+            (e) => renderContainer(
+              color: rainbowColors[e % rainbowColors.length],
+              index: e,
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  // 2. 보이는 것만 그림
+  Widget renderBuilderCrossAxisCount() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+      ),
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+      itemCount: 100,
+    );
+  }
+
+  // 3. 최대 사이즈
+  Widget renderMaxExtent() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+      ),
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+      itemCount: 100,
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      height: height ?? 300, // null이면 300 넣어라
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
