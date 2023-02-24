448541829d76e3ed660657671c3397e055a1320d
Hoseong Kim <8536048@gmail.com>
2023-02-24 12:00:00 +0900

    이론 Scrollable Widget

---
+++ cf/theory/scrollable_widget/lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widget/const/colors.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  CustomScrollViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('CustomScrollViewScreen'),
+          ),
+          renderBuilderSliverList(),
+        ],
+      ),
+    );
+  }
+
+  // ListView 기본 생성자와 비슷함
+  SliverList renderChildSliverList() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                color: rainbowColors[e % rainbowColors.length],
+                index: e,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  // ListView.builder 생성자와 비슷함
+  SliverList renderBuilderSliverList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        childCount: 100,
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      key: Key(index.toString()),
+      height: height ?? 300, // null이면 300 넣어라
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
