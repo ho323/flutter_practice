399e5b5c8812e641e02dc738d9e438ceda3f3441
Hoseong Kim <8536048@gmail.com>
2023-02-27 12:00:00 +0900

    이론 Scrollable Widget

--- cf/theory/scrollable_widget/lib/screen/custom_scroll_view_screen.dart
+++ cf/theory/scrollable_widget/lib/screen/custom_scroll_view_screen.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widget/const/colors.dart';
 
+class _SliverFixedHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+  final double maxHeight;
+  final double minHeight;
+
+  _SliverFixedHeaderDelegate({
+    required this.child,
+    required this.maxHeight,
+    required this.minHeight,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(
+      child: child,
+    );
+  }
+
+  @override
+  // 최대 높이
+  double get maxExtent => maxHeight;
+
+  @override
+  // 최소 높이
+  double get minExtent => minHeight;
+
+  @override
+  // covariant - 상속된 클래스도 사용가능
+  // oldDelegate - build 실행 됐을 때 이전 Delegate
+  // this - 새로운 Delegate
+  // shouldRebuild - 새로 build 해야 될지 말지 결정
+  bool shouldRebuild(_SliverFixedHeaderDelegate oldDelegate) {
+    return oldDelegate.minHeight != minHeight ||
+        oldDelegate.maxHeight != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
+
 class CustomScrollViewScreen extends StatelessWidget {
   final List<int> numbers = List.generate(100, (index) => index);
 
@@ -12,10 +51,34 @@ class CustomScrollViewScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           renderSliverAppbar(),
+          renderHeader(),
           renderBuilderSliverList(),
+          renderHeader(),
           renderSliverGridBuilder(),
+          renderHeader(),
           renderBuilderSliverList(),
         ],
+      ),
+    );
+  }
+
+  SliverPersistentHeader renderHeader() {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverFixedHeaderDelegate(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              'test',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        maxHeight: 250,
+        minHeight: 170,
       ),
     );
   }
