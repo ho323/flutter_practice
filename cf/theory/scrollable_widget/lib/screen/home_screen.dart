492b329742339809c035eeeadef9355210ea0eae
Hoseong Kim <8536048@gmail.com>
2023-03-01 04:41:19 +0900

    이론 Scrollable Widget

--- cf/theory/scrollable_widget/lib/screen/home_screen.dart
+++ cf/theory/scrollable_widget/lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:scrollable_widget/layout/main_layout.dart';
 import 'package:scrollable_widget/screen/custom_scroll_view_screen.dart';
 import 'package:scrollable_widget/screen/grid_view_screen.dart';
 import 'package:scrollable_widget/screen/list_view_screen.dart';
+import 'package:scrollable_widget/screen/refresh_indicator.dart';
 import 'package:scrollable_widget/screen/reorderable_list_view_screen.dart';
 import 'package:scrollable_widget/screen/scrollbar_screen.dart';
 import 'package:scrollable_widget/screen/single_child_scroll_view_screen.dart';
@@ -42,6 +43,10 @@ class HomeScreen extends StatelessWidget {
     ScreenModel(
       builder: (_) => ScrollbarScreen(),
       name: 'ScrollbarScreen',
+    ),
+    ScreenModel(
+      builder: (_) => RefreshIndicatorScreen(),
+      name: 'RefreshIndicatorScreen',
     ),
   ];
 
---
+++ cf/theory/scrollable_widget/lib/screen/refresh_indicator.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widget/const/colors.dart';
+import 'package:scrollable_widget/layout/main_layout.dart';
+
+class RefreshIndicatorScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  RefreshIndicatorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'RefreshIndicatorScreen',
+      body: RefreshIndicator(
+        onRefresh: () async {
+          // 서버 요청
+          await Future.delayed(Duration(seconds: 3));
+        },
+        child: ListView(
+          children: numbers
+              .map(
+                (e) => renderContainer(
+                  color: rainbowColors[e % rainbowColors.length],
+                  index: e,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      key: Key(index.toString()),
+      height: height ?? 300, // null이면 300 넣어라
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
